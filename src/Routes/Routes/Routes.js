844b2758338d8bdf6b1aeb3fa23693df27862e08
bikash-dev <bikash.dev.bk@gmail.com>
2022-10-19 07:01:39 +0600

    layouts and folder structure created

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,4 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
+import Category from "../../components/Category/Category/Category";
+import Home from "../../components/Home/Home/Home";
+import News from "../../components/News/News/News";
 import Main from "../../layouts/Main";
 
 export const routes = createBrowserRouter([
@@ -6,7 +9,18 @@ export const routes = createBrowserRouter([
         path: '/',
         element: <Main></Main>,
         children: [
-            
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/news/:id',
+                element: <News></News>
+            },
+            {
+                path: '/category/:id',
+                element: <Category></Category>
+            },
         ]
     }
 ])
